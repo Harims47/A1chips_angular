50c071b0416b00ca9e6e0ec3e3874dcf4814649c
Harinarayanan.A <8810060@INDUS.LOCAL>
2022-04-12 16:54:47 +0530

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoryComponent } from './category/category.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MenuComponent } from './menu/menu.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: DashboardComponent,
+
+  },
+  {
+    path: 'category',
+    component: CategoryComponent,
+
+  },
+  {
+    path: 'menu',
+    component: MenuComponent,
+
+  },
+  // {
+  //   path: 'dashboard',
+  //   component: DashboardComponent,
+
+  // },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CategoryComponent } from './category/category.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CategoryComponent,
+    DashboardComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
